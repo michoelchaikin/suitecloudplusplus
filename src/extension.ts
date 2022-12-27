65d1d765730d28100b16bd14edc027b1ce9c0a8a
Chidi Okwudire <stories@netsuite-insights.com>
2022-12-27 12:24:19 -0500

    Added support for workspace with multiple client projects.

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 import * as vscode from "vscode";
 
+import * as multiClient from "./multiClient";
 import * as multiEnvironment from "./multiEnvironment";
 import * as multiProject from "./multiProject";
 
 export function activate(context: vscode.ExtensionContext) {
-  multiEnvironment.activate(context);
+  multiClient.activate(context);
+  // multiEnvironment.activate(context);
   multiProject.activate(context);
 }
